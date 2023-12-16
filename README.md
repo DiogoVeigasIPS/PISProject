2bcaf74a0278916b9f3072f7ed7e14b970484cdd
Diogo Veigas <202200879@estudantes.ips.pt>
2023-12-16 16:23:50 +0000

    updated structure, added nodemon, basic index.js

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# PISProject
+# PsiProject
+
+*Developed by André Carvalho and Diogo Veigas as part of Programming and Integration of Services.*
+
+## Overview
+This comprehensive project comprises the following components:
+
+- **API**
+- **Web App**
+- **Database**
+- **2 Instruction Manuals**
+
+## Quick Start
+To expedite the setup process, ensure Node.js is installed, and follow these steps:
+
+```bash
+npm install
+npm start
+```
+
+## Project Status
+*Currently under active development.*
--- index.js
+++ index.js
@@ -2,3 +2,27 @@
  * Filename: index.js
  * Purpose: Contains all the basic logic of the application
  */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Body parser
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/static'));
+app.use(express.urlencoded({ extended: true }));
+
+// Mustache and views
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname, '/views'));
+
+// Use api and web app routers
+
+app.get('*', (req, res) => {
+    res.status(404).send("Page not found.");
+});
+
+app.listen(8081, () => {
+    console.log("listening to: http://localhost:8081");
+});
