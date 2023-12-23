084c6ac76ea2c73ff4d9aa5a5159201039853893
AndCvlh <202200878@estudantes.ips.pt>
2023-12-23 04:13:18 +0000

    ingredient description and categoryTemp

---
+++ api/models/category.js
@@ -0,0 +1,25 @@
+/**
+ * Represents a Category entity for interacting with the API.
+ *
+ * @class
+ */
+class Category {
+    /**
+     * Creates an instance of category.
+     *
+     * @constructor
+     * @param {Object} categoryData - The data representing a category.
+     * @param {int} category.id - The id of a category.
+     * @param {string} category.name - The name of a category.
+     * @param {string} category.description - The description of a category.
+     * @param {string} category.image - The image of a category.
+     */
+    constructor(categoryData) {
+        this.id = categoryData.id;
+        this.name = categoryData.name;
+        this.description = categoryData.category;
+        this.image = categoryData.image;    
+    }
+}
+
+module.exports = Category;
--- api/models/ingredient.js
+++ api/models/ingredient.js
@@ -10,10 +10,12 @@ class Ingredient {
      * @constructor
      * @param {Object} ingredientData - The id of a recipe in the provider's API.
      * @param {string} ingredient.name - The id of a recipe in the provider's API.
+     * @param {string} ingredient.description - The description of a ingredient.
      * @param {string} ingredient.quantity - The name of a recipe.
      */
     constructor(ingredientData) {
         this.name = ingredientData.name;
+        this.description = ingredientData.description;
         this.quantity = ingredientData.quantity;
     }
 }
