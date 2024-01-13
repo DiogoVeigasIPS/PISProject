6b2fe04d358fcb9481882002e503887962463a23
AndCvlh <202200878@estudantes.ips.pt>
2024-01-13 22:15:30 +0000

    back ingredients

--- app/admin/index.js
+++ app/admin/index.js
@@ -3,7 +3,7 @@
  * Purpose: Manages the application's admin part workflow.
  */
 const express = require('express');
-const { recipeActions } = require('../../api/actions');
+const { recipeActions, ingredientActions } = require('../../api/actions');
 
 const router = express.Router();
 
@@ -11,5 +11,11 @@ router.get('', async (req, res) => {
     const recipes = (await recipeActions.getRecipes()).responseMessage;
     res.render('backRecipes', { recipes: recipes });
 });
+
+router.get('/ingredients', async (req, res) => {
+    const ingredients = (await ingredientActions.getIngredients()).responseMessage;
+    res.render('partials/ingredients', { ingredients: ingredients});
+});
+
 
 module.exports = router;
