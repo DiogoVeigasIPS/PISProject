352f7d2a48c0fc488d42ee0a2a33ef2a7f98f8b5
Diogo Veigas <202200879@estudantes.ips.pt>
2024-01-13 18:29:33 +0000

    admin and nav footer basic routing

--- app/admin/index.js
+++ app/admin/index.js
@@ -3,6 +3,13 @@
  * Purpose: Manages the application's admin part workflow.
  */
 const express = require('express');
+const { recipeActions } = require('../../api/actions');
+
 const router = express.Router();
+
+router.get('', async (req, res) => {
+    const recipes = (await recipeActions.getRecipes()).responseMessage;
+    res.render('backRecipes', { recipes: recipes });
+});
 
 module.exports = router;
