95b78883d6fcdc657487cc3051dd3c905b440e51
AndCvlh <202200878@estudantes.ips.pt>
2024-01-14 17:45:38 +0000

    backIngredient pages and db changes

--- app/admin/index.js
+++ app/admin/index.js
@@ -13,8 +13,53 @@ router.get('', async (req, res) => {
 });
 
 router.get('/ingredients', async (req, res) => {
-    const ingredients = (await ingredientActions.getIngredients()).responseMessage;
-    res.render('partials/ingredients', { ingredients: ingredients});
+    const stringSearch = req.query.name || null;
+    const order = req.query.sort || null;
+    const by = req.query.by == 'asc' ? 'desc' : 'asc';
+    const queryOptions = {
+        stringSearch: stringSearch,
+        order: order,
+        by: by
+    };
+
+    const ingredients = (await ingredientActions.getIngredients(queryOptions)).responseMessage;
+
+    res.render('backIngredients', { ingredients: ingredients });
+});
+
+
+router.get('/ingredient/details/:id', async (req, res) => {
+    const id = req.params.id;
+    const ingredient = (await ingredientActions.getIngredient(id)).responseMessage;
+
+    res.render('backIngredientDetails', { ingredient: ingredient });
+});
+
+router.get('/ingredient/edit/:id', async (req, res) => {
+    const id = req.params.id;
+    const ingredient = (await ingredientActions.getIngredient(id)).responseMessage;
+
+    res.render('backIngredientCreateDelete', { ingredient: ingredient });
+});
+
+router.get('/ingredient/create/:id', async (req, res) => {
+    const id = req.params.id;
+    const ingredient = (await ingredientActions.getIngredient(id)).responseMessage;
+
+    res.render('backIngredientCreateDelete', { ingredient: ingredient });
+});
+
+// In your admin.js or wherever you define your routes
+router.delete('/ingredients/delete/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        await ingredientActions.deleteIngredient(id).responseMessage;;
+        res.sendStatus(204); // Successfully deleted
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to delete ingredient.' });
+    }
+
 });
 
 
