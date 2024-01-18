ace3bf664c779bd4ee7a0305f7d04565bc40b8cc
Diogo Veigas <202200879@estudantes.ips.pt>
2024-01-18 16:44:26 +0000

    add ingredient through modal edit ingredient through modal create and edit use api still realod, no dom add or edit table

--- static/js/auxIngredients.js
+++ static/js/auxIngredients.js
@@ -3,69 +3,6 @@
  * Purpose: To fetch the add, edit and delete from the ingredientActions and ask if the user is sure about the deletion
  */
 
-const backDeleteIngredient = async (id) => {
-    try {
-        const confirmed = confirm("Are you sure you want to delete this ingredient?");
-        if (!confirmed) {
-            return;
-        }
-
-        // Use fetch to send a DELETE request to your existing API endpoint
-        const response = await fetch(`/admin/ingredient/${id}`, {
-            method: 'DELETE',
-        });
-        if (response.ok) {
-            // Successful deletion
-            console.log("Ingredient deleted successfully.");
-            location.reload();
-        } else {
-            // Handle errors
-            console.error("Failed to delete ingredient.");
-        }
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-const ingredientForm = document.querySelector('#ingredientForm');
-if (ingredientForm) {
-    ingredientForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-
-        const name = ingredientForm.querySelector('input[name="name"]').value;
-        const description = ingredientForm.querySelector('textarea[name="description"]').value;
-        const image = ingredientForm.querySelector('input[name="image"]').value;
-
-        const options = {
-            method: "POST",
-            body: JSON.stringify({ name, description, image }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
-
-        try {
-            const response = await fetch("http://localhost:8081/admin/ingredient/create/", options);
-
-            let responseData = null;
-            const errorDiv = ingredientForm.querySelector('#ingredientError');
-
-            responseData = await response.json();
-
-            if (responseData?.statusCode === 200) {
-                errorDiv.classList.add('d-none');
-                location.href = "http://localhost:8081/admin/ingredients";
-            } else {
-                errorDiv.classList.remove('d-none');
-                errorDiv.innerHTML = responseData.responseMessage;
-            }
-
-        } catch (error) {
-            console.error("Error during ingredient addition:", error);
-        }
-    });
-}
-
 const openIngredientDetailsModal = async (id) => {
     try {
         const response = await fetch(`http://localhost:8081/api/ingredient/${id}`);
@@ -93,6 +30,126 @@ const openIngredientDetailsModal = async (id) => {
     } catch (err) {
         console.error(err)
     }
+}
+
+const openAddIngredientModal = async () => {
+    const ingredientNameForm = document.getElementById('ingredientNameForm');
+    const ingredientDescriptionForm = document.getElementById('ingredientDescriptionForm');
+    const ingredientImageInputForm = document.getElementById('ingredientImageInputForm');
+    const imagePreview = document.getElementById('imagePreview');
+
+    ingredientNameForm.value = ingredientDescriptionForm.value = ingredientImageInputForm.value = imagePreview.src = "";
+
+    const submitIngredientButton = document.getElementById('submitIngredientButton');
+    submitIngredientButton.innerText = 'Add Ingredient';
+
+    const activityTitle = document.getElementById('activityTitle');
+    activityTitle.innerText = 'Add an ingredient';
+
+    const ingredientForm = document.querySelector('#ingredientForm');
+    ingredientForm.onsubmit = async (e) => {
+        e.preventDefault();
+
+        const name = ingredientNameForm.value;
+        const description = ingredientDescriptionForm.value;
+        const image = ingredientImageInputForm.value;
+
+        const options = {
+            method: "POST",
+            body: JSON.stringify({ name, description, image }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        try {
+            const response = await fetch("http://localhost:8081/api/ingredient", options);
+            
+            const errorDiv = ingredientForm.querySelector('#ingredientError');
+
+            if (response.ok) {
+                const responseData = await response.json();
+                errorDiv.classList.add('d-none');
+                // Add through DOM
+                location.href = "http://localhost:8081/admin/ingredients";
+            } else {
+                const responseData = await response.text();
+                errorDiv.classList.remove('d-none');
+                errorDiv.innerHTML = responseData;
+            }
+
+        } catch (error) {
+            console.error("Error during ingredient addition:", error);
+        }
+    };
+
+    const ingredientDetails = new bootstrap.Modal(document.getElementById('ingredientFormModal'));
+    ingredientDetails.show();
+}
+
+const openEditIngredientModal = async (id) => {
+    const ingredientNameForm = document.getElementById('ingredientNameForm');
+    const ingredientDescriptionForm = document.getElementById('ingredientDescriptionForm');
+    const ingredientImageInputForm = document.getElementById('ingredientImageInputForm');
+    const imagePreview = document.getElementById('imagePreview');
+
+    const activityTitle = document.getElementById('activityTitle');
+
+    try {
+        const response = await fetch(`http://localhost:8081/api/ingredient/${id}`);
+        if (response.ok) {
+            const responseData = await response.json();
+            activityTitle.innerHTML = `Edit <strong>${responseData.name}</strong>`;
+            ingredientNameForm.value = responseData.name;
+            ingredientDescriptionForm.value = responseData.description;
+            ingredientImageInputForm.value = responseData.image;
+            imagePreview.src = responseData.image;
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
+    const submitIngredientButton = document.getElementById('submitIngredientButton');
+    submitIngredientButton.innerText = 'Edit Ingredient';
+
+    const ingredientForm = document.querySelector('#ingredientForm');
+    ingredientForm.onsubmit = async (e) => {
+        e.preventDefault();
+
+        const name = ingredientNameForm.value;
+        const description = ingredientDescriptionForm.value;
+        const image = ingredientImageInputForm.value;
+
+        const options = {
+            method: "PUT",
+            body: JSON.stringify({ name, description, image }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        try {
+            const response = await fetch(`http://localhost:8081/api/ingredient/${id}`, options);
+
+            const errorDiv = ingredientForm.querySelector('#ingredientError');
+
+            if (response.ok) {
+                const responseData = await response.json();
+                errorDiv.classList.add('d-none');
+                location.href = "http://localhost:8081/admin/ingredients";
+            } else {
+                const responseData = await response.text();
+                errorDiv.classList.remove('d-none');
+                errorDiv.innerHTML = responseData;
+            }
+
+        } catch (error) {
+            console.error("Error during ingredient addition:", error);
+        }
+    };
+
+    const ingredientDetails = new bootstrap.Modal(document.getElementById('ingredientFormModal'));
+    ingredientDetails.show();
 }
 
 const openDeleteModal = (id, name) => {
