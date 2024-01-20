07f70b203384355e649b4396d5a6948ebdab6185
Diogo Veigas <202200879@estudantes.ips.pt>
2024-01-20 15:28:39 +0000

    api add see and remove favorite recipes from user

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -9,7 +9,11 @@ const { verifyJWT } = require('../jsonWebToken');
 
 const router = express.Router();
 
+router.get('/:id/favoriteRecipe', userController.getFavorites);
+router.post('/:id/favoriteRecipe/:recipe_id', userController.addFavorite);
+router.delete('/:id/favoriteRecipe/:recipe_id', userController.removeFavorite);
 router.get('/loggedIn', verifyJWT, userController.userIsLoggedIn);
+
 router.get('', userController.readUsers);
 router.get('/:id', userController.readUser);
 router.post('', userController.addUser);
