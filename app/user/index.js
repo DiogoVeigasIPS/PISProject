a43cd3a1a3bdf78dac248c3eeeba8712b66ffdfc
Diogo Veigas <202200879@estudantes.ips.pt>
2024-01-21 19:25:19 +0000

    favorite recipe timestamp protect favorite recipes show favorite recipes homepage user permission management (admin can access other users, but user can only access its own) max fav recipes

--- app/user/index.js
+++ app/user/index.js
@@ -17,60 +17,6 @@ router.get('/me', async (req, res) => {
     res.render('userPage',);
 });
 
-router.post('/submit-recipe', async (req, res) => {
-    const body = req.body;
-
-    const ingredients = [];
-
-    // Add the ids and quantities
-    for (let i = 0; i < body.quantities.length; i++) {
-        ingredients.push(new IngredientInRecipe({
-            ingredient: new Ingredient({ id: body.ingredientIds[i] }),
-            quantity: body.quantities[i]
-        }))
-    }
-
-    const recipe = new Recipe({
-        id: null,
-        name: body.name,
-        category: new Category({ id: body.category, name: null, description: null, image: null }),
-        description: body.description,
-        preparationDescription: body.preparationDescription,
-        area: new Area({ id: body.area, name: null }),
-        author: new Author({ id: null, username: null, firstName: null, lastName: null }),
-        image: body.image,
-        preparationTime: body.preparationTime,
-        difficulty: new Difficulty({ id: body.difficulty, name: null }),
-        cost: body.cost,
-        ingredients: ingredients
-    });
-
-    try {
-        const newRecipe = await recipeActions.addRecipe(recipe);
-        res.redirect(`/recipe?id=${newRecipe.responseMessage.id}`);
-    } catch (error) {
-        console.error(error)
-        res.status(500).send(error);
-    }
-
-    //res.json(body);
-});
-
-router.get('/add-recipe', async (req, res) => {
-    const categories = await categoryActions.getCategories();
-    const areas = await areaActions.getAreas();
-    const difficulties = await difficultyActions.getDifficulties();
-
-    const renderOptions = {
-        title: "Adding a recipe",
-        categories: categories.responseMessage,
-        areas: areas.responseMessage,
-        difficulties: difficulties.responseMessage
-    }
-
-    res.render('recipeForm', renderOptions);
-});
-
 // Details Page
 router.get('/recipe/:id', async (req, res) => {
     const queryId = req.params.id;
